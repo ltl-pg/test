820948c6fecbd15a3bdd1c035cf693be65054d63
ltl-pg <hashi.works1@gamil.com>
2025-06-05 10:01:24 +0900

    test

--- src/main/java/com/example/music_management/MusicManagementApplication.java
+++ src/main/java/com/example/music_management/MusicManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MusicManagementApplication {
-// test
+// tesy
 	public static void main(String[] args) {
 		SpringApplication.run(MusicManagementApplication.class, args);
 	}
