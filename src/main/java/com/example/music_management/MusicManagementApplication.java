c70e7f7527953ba2afa51a8f33c9903fe20f93be
tmister <cloudstrife7777@gamil.com>
2025-06-05 10:03:47 +0900

    test

--- src/main/java/com/example/music_management/MusicManagementApplication.java
+++ src/main/java/com/example/music_management/MusicManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MusicManagementApplication {
-// tesy
+// test
 	public static void main(String[] args) {
 		SpringApplication.run(MusicManagementApplication.class, args);
 	}
